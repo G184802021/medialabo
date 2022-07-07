cf23d552f8052971ef4ee76baeb5f03160a994b8
G184802021 <218480@st.takushoku-u.ac.jp>
2022-07-07 15:01:12 +0900

    7/7

--- kazuate.js
+++ kazuate.js
@@ -7,35 +7,28 @@ let kaisu = 0;
 var ha = false;
 // 予想を4回実行する
 // 将来: ボタンを押したら， hantei() を呼び出すように修正する
-hantei();
-//hantei();
-//hantei();
-//hantei();
+let b = document.querySelector('#print');
+b.addEventListener('click', hantei)
 
 // ボタンを押した後の処理をする関数 hantei() の定義
 function hantei() {
-    let yoso = 4;       // 第5回課題:テキストボックスの数値をここに代入
+    let i = document.querySelector('input[name="yosou"]');
+    let yoso = parseInt(i.value);       // 第5回課題:テキストボックスの数値をここに代入
     // 課題3-1：ここの判定処理を作成する．
     //        ページに表示する方法はまだ習っていないので
     //        判定結果はコンソールに出力すること
     kaisu++;
-    let kai = kaisu;
-    let yo =yoso;
     let s = document.querySelector('span#kaisu');
-    s.textContent = kai;
+    s.textContent = kaisu;
     s = document.querySelector('span#yoso');
     s.textContent = yoso;
     let result = "";
     s = document.querySelector('span#result');
     if(ha){
         result="答えは "+kotae+" でした．すでにゲームは終わっています";
-        
-   
-    }
-    else if(yoso === kotae){
+    } else if(yoso === kotae){
         result="正解です．おめでとう!";
         ha= true;
-        
     } else {
         if(kaisu === 3){
             result="まちがい．残念でした答えは "+kotae+" です";
